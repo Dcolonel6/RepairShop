e0a8e628fde4fb60a329dd21d2fc5bb6d77e74a0
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-10-07 12:16:32 +0300

    added templates for form generation

--- src/Components/Pages/Users.js
+++ src/Components/Pages/Users.js
@@ -4,3 +4,47 @@ import Modal from "./Reusable/Modal";
 import Table from "./Reusable/Table";
 import { FactoryServerCommunication } from "./Reusable/helpers/index"
 
+
+const template = {
+  fields: [
+    
+    {
+      tag: "input",
+      type: "text",
+      name: "fullName",
+      labelName: "Full Names",
+      choices: [],
+      dimensions: [],
+    },
+    {
+      tag: "input",
+      type: "email",
+      name: "emailAddress",
+      labelName: "Email Address",
+      choices: [],
+      dimensions: [],
+    },
+    {
+      tag: "select",
+      type: "select",
+      name: "gender",
+      labelName: "Sex",
+      choices: [
+        'Male',
+        'Female'
+      ],
+      dimensions: [],
+    },
+    {
+      tag: "select",
+      type: "select",
+      name: "isAdmin",
+      labelName: "Is the user an admin?",
+      choices: [
+        'Yes',
+        'No' 
+      ],
+      dimensions: [],
+    },
+  ],
+};
