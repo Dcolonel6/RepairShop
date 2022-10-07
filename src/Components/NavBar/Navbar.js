b9152b7179934d643104900d2b949abdb5dabd5c
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-10-07 11:56:19 +0300

    a navbar that toggles

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -5,3 +5,25 @@ import NavMenu from "./NavMenu";
 import { IconContext } from "react-icons";
 import "./NavBar.css";
 
+const NavBar = () => {
+  const [showSideBar, setShowSideBar] = useState(false);
+  const toggleSideBar = () => {
+    setShowSideBar((currentVal) => !currentVal);
+  };
+  return (
+    <div className="nav">
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <div className="navbar">
+          <Link to="#" className="menu-bars">
+            <IOIcons.IoIosMenu onClick={toggleSideBar} />
+          </Link>
+        </div>
+        <nav className={showSideBar ? "nav-menu active" : "nav-menu"}>
+          <NavMenu toggleSideBar={toggleSideBar} />
+        </nav>
+      </IconContext.Provider>
+    </div>
+  );
+};
+
+export default NavBar;
