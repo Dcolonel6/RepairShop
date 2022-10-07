03befdad9a04bdff4c77b739fef0d609a0ce475a
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-10-07 11:59:32 +0300

    imported my component dependancies

--- src/Components/Pages/Phones.js
+++ src/Components/Pages/Phones.js
@@ -1,4 +1,7 @@
 import React, {useEffect} from "react";
+import Form from "./Reusable/Forms";
+import Modal from "./Reusable/Modal";
+import Table from "./Reusable/Table";
 const Phones = () => {
   return (
       <div>Phones</div>
