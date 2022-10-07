7bc92ddd025553a11c22c77dc1c871cc1d9af418
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-10-07 11:52:32 +0300

    looped through my menu links while building list items nesting Links.

--- src/Components/NavBar/NavMenu.js
+++ src/Components/NavBar/NavMenu.js
@@ -2,6 +2,7 @@ import React from 'react'
 import * as IoIcons from 'react-icons/io5'
 import * as MdIcons from 'react-icons/md'
 import * as HiIcons from "react-icons/hi";
+import { Link } from 'react-router-dom'
 
 
 const menuItems =  [
@@ -31,3 +32,27 @@ const menuItems =  [
     },    
     
 ]
+
+const NavMenu = ({toggleSideBar}) => {
+    const liOfMenuItems = menuItems.map(({title,path,icon,klass}, index) => {
+        return (
+            <li key={index} className={klass} >
+                <Link to={path} >
+                    <span>{icon}</span>{title}
+                </Link>
+            </li>            
+        );
+    })
+  return (
+    <ul className="nav-menu-items" onClick={toggleSideBar}>
+        <li className="navbar-toggle">
+            <Link to="#" className="menu-bars">
+                <IoIcons.IoCloseOutline />
+            </Link>
+        </li>
+        {liOfMenuItems}
+    </ul>
+  );
+}
+
+export default NavMenu
