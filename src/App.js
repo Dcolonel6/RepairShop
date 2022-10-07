f103cd51ec119bbf7946aebc73ff7d2b3d48378a
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-10-07 11:45:53 +0300

    imported all my dependancies for App.js

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import NavBar from "./Components/NavBar/Navbar";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./Components/Pages/Home/Home";
+import Phones from "./Components/Pages/Phones";
+import Users from "./Components/Pages/Users";
+import Tickets from "./Components/Pages/Tickets";
+
