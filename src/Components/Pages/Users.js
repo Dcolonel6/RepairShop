59dfedc1fadabb6acedf12853ac6aa0e31b852d1
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-10-07 12:16:04 +0300

    imported all dependancies

---
+++ src/Components/Pages/Users.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import Form from "./Reusable/Forms";
+import Modal from "./Reusable/Modal";
+import Table from "./Reusable/Table";
+import { FactoryServerCommunication } from "./Reusable/helpers/index"
+
