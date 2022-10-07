fe1da061ecc4d45a15ee67e34d957d285421361d
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-10-07 11:55:47 +0300

    imported dependancies for my nabar

---
+++ src/Components/NavBar/Navbar.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import * as IOIcons from "react-icons/io";
+import NavMenu from "./NavMenu";
+import { IconContext } from "react-icons";
+import "./NavBar.css";
+
