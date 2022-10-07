dab58a82f7699b7956d2edf8ee5354362cd0b01f
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-10-07 11:46:32 +0300

    set up the main routes

--- src/App.js
+++ src/App.js
@@ -5,3 +5,29 @@ import Phones from "./Components/Pages/Phones";
 import Users from "./Components/Pages/Users";
 import Tickets from "./Components/Pages/Tickets";
 
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="">
+        <NavBar />
+        <Switch>
+          <Route path="/" exact>
+            <Home/>
+          </Route>
+          <Route path="/phones">
+            <Phones />
+          </Route>
+          <Route path="/teams">
+            <Users />
+          </Route>
+          <Route path="/tickets">
+            <Tickets />
+          </Route> 
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
